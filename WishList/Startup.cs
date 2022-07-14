8419392463f4a6528013d67a57ca5ce2c35aaecd
venugopal.vanam5@gmail.com <venugopal.vanam5@gmail.com>
2022-07-14 11:08:09 +0000

    call for endpoint

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -36,7 +36,8 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-             app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
