b7437385701225738f90dfbdbc681ca17d077c21
M1082271 <M1082271@MT-5CG1478XTN>
2022-07-15 10:22:57 +0530

    for viewstart

--- WishList/Views/_ViewStart.cshtml.cs
+++ WishList/Views/_ViewStart.cshtml.cs
@@ -1,1 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.views
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
 
