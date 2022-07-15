7596fbae06775cd962f86d4f672b347b22a29b49
M1082271 <M1082271@MT-5CG1478XTN>
2022-07-15 10:17:18 +0530

    cshtml.cs for create

--- WishList/Views/Item/Create.cshtml.cs
+++ WishList/Views/Item/Create.cshtml.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace WishList.views.Item
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
