47b747d9a9164f7a480b46835009afd0112ff643
M1082271 <M1082271@MT-5CG1478XTN>
2022-07-15 10:21:19 +0530

    for viewimports

--- WishList/Views/_ViewImports.cshtml.cs
+++ WishList/Views/_ViewImports.cshtml.cs
@@ -1,1 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace WishList.views
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
